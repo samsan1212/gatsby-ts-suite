a0966dc2017df79e49638f2cd6027b1010f90e28
Tsz San Sam, CHAN <samchan148@gmail.com>
2020-09-22 11:53:18 +0800

    Update helper.ts
    Add `toActionCreator` function overload, in order to have a better type-safe action creator

--- src/redux/helper.ts
+++ src/redux/helper.ts
@@ -1,16 +1,21 @@
-import { Reducer, ActionCreator } from "redux";
+import { Reducer, ActionCreator, Action } from "redux";
 
 export function toReducer<T>(type: string, defaultValue: T): Reducer<T, AppAction<T>> {
     return (state = defaultValue, action) => {
-        if (action && action.type === type) {
+        if (action && action.type === type && action.payload) {
             return action.payload;
         }
         return state;
     };
 }
 
-export function toActionCreator<T = {}>(type: string): ActionCreator<AppAction<T>> {
-    return (payload: T = ({} as any)) => ({ type, payload });
+export function toActionCreator(type: string, noPayload: true): ActionCreator<Action<string>>
+export function toActionCreator<T>(type: string): ActionCreator<AppAction<T>>
+export function toActionCreator<T>(type: string, noPayload = false) {
+    if (noPayload) {
+        return () => ({ type })
+    }
+    return (payload: T) => ({ type, payload });
 }
 
-export const toType = (prefix: string) => (actionName: string) => `${prefix}::${actionName}`;
+export const toType = (prefix: string) => (actionName: string): string => `${prefix}::${actionName}`;
