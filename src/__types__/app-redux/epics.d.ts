62a7371ef53acd95dc54bdee2ab3618f8f46328c
Sam Tsz San, CHAN <samchan148@gmail.com>
2021-03-22 18:10:06 +0800

    chore: add back missing type

--- src/__types__/app-redux/epics.d.ts
+++ src/__types__/app-redux/epics.d.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Epic } from "redux-observable"
+import { AppAction } from "redux-functions"
 
 export interface PageStoreWithEpic {
   epicMounted: boolean
 }
 
 declare global {
-  type AppEpic<ActionPayload = any> = Epic<
+  type AppEpic<ActionPayload = unknown> = Epic<
     AppAction<ActionPayload>,
     any,
     ReduxStore,
