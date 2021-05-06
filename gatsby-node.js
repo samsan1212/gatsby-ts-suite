b528adde19740f65a8589b0d472c25f5e4d435d0
Sam Chan <samchan148@gmail.com>
2021-05-06 16:00:02 +0800

    chore(gatsby-node): use esbuild instead of babel for transpiling ts/js

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
+const { ProvidePlugin } = require("webpack");
+const esbuild = require("esbuild");
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+      optimization: {
+        minimize: false,
+        minimizer: [
+          new ESBuildMinifyPlugin({
+            target: "es2017",
+            implementation: esbuild
+          }),
+        ],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(j|t)sx?$/,
+            loader: "esbuild-loader",
+            options: {
+              loader: "tsx",
+              tsconfigRaw: require("./tsconfig.json"),
+              implementation: esbuild
+            },
+          },
+        ],
+      },
+      plugins: [
+        new ProvidePlugin({
+          React: "react",
+        }),
+      ],
+    });
+  };
